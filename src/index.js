38c4b8d0df518f3894815f7bb267bc01809831f7
jbd95 <jbrady9559@gmail.com>
2019-02-23 12:56:17 -0600

    Update with Webhooks

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const childProcess = require('child_process')
+
+app.listen(port, () => console.log('Listening on port', port))
+
+app.get('/truerev/', (req, res) => res.send('Hello World'))
+
+app.post('webhooks/github', (req, res) => {
+    var sender = req.body.sender;
+    var branch = req.body.ref;
+
+    if (branch.indexOf('master') > -1 && sender.login != "") {
+        deploy(res)
+    }
+})
+
+app.post('/truerev/test', (req, res) => res.send("Testing good"))
+
+const deploy = (res) => {
+    childProcess.exec('cd /home && ./deploy.sh', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err)
+            return res.send(500)
+        }
+        res.send(200)
+    })
+}
