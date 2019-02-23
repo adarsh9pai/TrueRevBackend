e76f9f64a65ec50c63f970b7a9150fe89e4e9678
jbd95 <jbrady9559@gmail.com>
2019-02-23 13:02:15 -0600

    Update with Webhooks

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.post('/webhooks/github', (req, res) => {
     }
 })
 
-app.get('/truerev/testing', (req, res) => res.send("Testing good"))
+app.get('/truerev/test', (req, res) => res.send("Testing good"))
 
 const deploy = (res) => {
     childProcess.exec('cd /home && ./deploy.sh', (err, stdout, stderr) => {
