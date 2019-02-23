2dca26fb9acc9934f4a32b5b282d67daaff81730
jbd95 <jbrady9559@gmail.com>
2019-02-23 14:24:46 -0600

    Send Json

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const childProcess = require('child_process')
+const productSearch = require('./products')
 
 app.listen(port, () => console.log('Listening on port', port))
 
-app.get('/truerev/', (req, res) => res.send('Hello World'))
+app.get('/truerev/', (req, res) => res.json({ message: 'Hello World' }))
+
+app.get('/truerev/random', (req, res) => res.send("Testing good"))
 
 app.post('/webhooks/github', (req, res) => {
     console.log("github update called")
@@ -14,8 +17,6 @@ app.post('/webhooks/github', (req, res) => {
 
     deploy(res)
 })
-
-app.get('/truerev/random', (req, res) => res.send("Testing good"))
 
 const deploy = (res) => {
     childProcess.exec('cd /home && ./deploy.sh', (err, stdout, stderr) => {
